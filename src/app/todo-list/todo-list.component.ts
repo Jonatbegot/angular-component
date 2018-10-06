bdf21049d842ba4a95ba35e4854c0190d3cc3899
maxdhn <maxduchan@gmail.com>
2018-10-06 18:43:26 +0200

    ngDoCheck

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, OnChanges, SimpleChanges, Input} from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent implements OnInit, OnChanges {
+export class TodoListComponent implements OnInit, /* OnChanges, */ DoCheck {
 
   @Input()
   name: string;
@@ -16,8 +16,13 @@ export class TodoListComponent implements OnInit, OnChanges {
     console.log(`ngOnInit value of name ${this.name}`);
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(`ngOnChanges value of changes ${changes['name'].currentValue}`);
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log(`ngOnChanges value of changes ${changes['name'].currentValue}`);
+  // }
+
+  ngDoCheck(): void {
+    console.log(`ngDoCheck changes ${this.name}`);
   }
+
 
 }
